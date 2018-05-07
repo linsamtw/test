afa121afb9fb03e82175a9ad5c2ff560fbdbfc73
lin sam <610411102@ems.ndhu.edu.tw>
2018-05-07 16:00:43 +0800

    Add files via upload

--- FinancialOpenData/load_data.py
+++ FinancialOpenData/load_data.py
@@ -34,23 +34,20 @@ def execute_sql2(host,user,password,database,sql_text):
         conn.close()
         return ''
 #---------------------------------------------------------
-''' test
-FS = FinancialStatements()
-data = FS.load('2330')# 讀取 2330 歷史財報
-data = FS.load_all()# 讀取 '所有股票' 歷史財報
-
-'''    
-class FinancialStatements:
-    def __init__(self):
+# based class 
+class LoadDate:
+    def __init__(self,database='',data_name=''):
+        self.data_name = data_name        
+        self.database = database   
         tem = execute_sql2(
                 host = host,
                 user = user,
                 password = password,
-                database = 'Financial_DataSet',
-                sql_text = 'SELECT distinct `stock_id` FROM `FinancialStatements`')
+                database = self.database,
+                sql_text = 'SELECT distinct `stock_id` FROM `' + self.data_name + '`')
 
         self.stock_cid = [ te[0] for te in tem ]        
-        #self.stock = stock
+
         
     def check_stock(self):
         tem = list( set([self.stock]) & set(self.stock_cid) )
@@ -59,100 +56,106 @@ class FinancialStatements:
             return 0
         else:
             return 1
-        
-    def get_col_name(self,database,data_name):
+
+    def get_col_name(self):
        
         tem_col_name = execute_sql2(
                 host = host,
                 user = user,
                 password = password,
-                database = database,
-                sql_text = 'SHOW columns FROM '+ data_name )
+                database = self.database,
+                sql_text = 'SHOW columns FROM '+ self.data_name )
     
         col_name = []
         for i in range(len(tem_col_name)):
             col_name.append( tem_col_name[i][0] )
         col_name.remove('id')    
         self.col_name = col_name
+    
+    def execute2sql(self,text,col):
+        tem = execute_sql2(
+            host = host,
+            user = user,
+            password = password,
+            database = self.database,
+            sql_text = text)
         
+        if col=='Date':
+            tem = [np.datetime64(x[0]) for x in tem]
+            tem = pd.DataFrame(tem)
+            self.data[col] = tem.loc[:,0]
+        else:
+            tem = np.concatenate(tem, axis=0)
+            tem = pd.DataFrame(tem)
+            self.data[col] = tem.T.iloc[0]
+    
+    def get_data(self,all_data = ''):
+        
+        self.data = pd.DataFrame()
+        for j in range(len(self.col_name)):
+            print(j)
+            col = self.col_name[j]
+            text = 'select ' + col + ' from ' + self.data_name
+            
+            if all_data == 'T': 
+                123
+            else:
+                text = text + " WHERE `stock_id` LIKE '"+str(self.stock)+"'"
+                
+            self.execute2sql(text,col)
+
+        return self.data
+            
     def load(self,stock):
         
-        self.stock = stock
+        self.stock = str( stock )
         if self.check_stock() ==0:
             return ''
         
-        self.get_col_name('Financial_DataSet','FinancialStatements')
-
-        data = pd.DataFrame()
-        for j in range(len(self.col_name)):
-            print(j)
-            col = self.col_name[j]
-            text = 'select ' + col + ' from ' + 'FinancialStatements'
-            text = text + " WHERE `stock_id` LIKE '"+str(self.stock)+"'"
-
-            tem = execute_sql2(
-                host = host,
-                user = user,
-                password = password,
-                database = 'Financial_DataSet',
-                sql_text = text)
-            
-            if col=='Date':
-                tem = [np.datetime64(x[0]) for x in tem]
-                tem = pd.DataFrame(tem)
-                data[col] = tem.loc[:,0]
-            else:
-                tem = np.concatenate(tem, axis=0)
-                tem = pd.DataFrame(tem)
-                data[col] = tem.T.iloc[0]
+        self.get_col_name()
+        data = self.get_data()
                 
         return data
     
+''' test FinancialStatements
+
+FS = FinancialStatements()  
+data = FS.load('2330')# 讀取 2330 歷史財報
+data = FS.load_all()# 讀取 '所有股票' 歷史財報
+
+'''    
+        
+class FinancialStatements(LoadDate):
+    def __init__(self):
+        super(FinancialStatements, self).__init__(
+                database = 'Financial_DataSet',
+                data_name = 'FinancialStatements')
+        
     def load_all(self):
         
-        self.get_col_name('Financial_DataSet','FinancialStatements')
+        self.get_col_name()
+        self.data = self.get_data(all_data='T')
 
-        data = pd.DataFrame()
-        for j in range(len(self.col_name)):
-            print(j)
-            col = self.col_name[j]
-            text = 'select ' + col + ' from ' + 'FinancialStatements'
-
-            tem = execute_sql2(
-                host = host,
-                user = user,
-                password = password,
-                database = 'Financial_DataSet',
-                sql_text = text)
-            
-            if col=='Date':
-                tem = [np.datetime64(x[0]) for x in tem]
-                tem = pd.DataFrame(tem)
-                data[col] = tem.loc[:,0]
-            else:
-                tem = np.concatenate(tem, axis=0)
-                tem = pd.DataFrame(tem)
-                data[col] = tem.T.iloc[0]
-                
-        return data
+        return self.data
         
 #-------------------------------------------------------------
-''' test
-SP = StockPrice()
+''' test StockPrice
+
+SP = StockPrice()  
 data = SP.load('2330')
 
 '''
-class StockPrice(FinancialStatements):
+class StockPrice(LoadDate):
     #---------------------------------------------------------------    
-    def __init__(self):                        
+    def __init__(self):    
+        self.database = 'StockPrice'
         tem = execute_sql2(host = host,user = user,password = password,
-                           database = 'StockPrice',sql_text = 'SHOW TABLES')
+                           database = self.database,
+                           sql_text = 'SHOW TABLES')
         #---------------------------------------------------------------                         
-        stock_id = []
-        stock_cid = []
+        stock_id,stock_cid = [],[]
         for d in tem:
-            d = d[0][1:]
-            d = d.replace('_','.')
+            d = d[0][1:].replace('_','.')
             stock_id.append( d )
             stock_cid.append( d.split('.')[0] )
     
@@ -160,7 +163,8 @@ class StockPrice(FinancialStatements):
                 'stock_id' : stock_id,
                 'stock_cid' : stock_cid})
         #---------------------------------------------------------------
-    def check_stock(self):
+
+    def change_stock_name(self):
         try:
             tem = self.stock_id[ self.stock_id['stock_cid'] == str(self.stock)].stock_id
             data_name = tem[ tem.index[0] ]
@@ -172,72 +176,55 @@ class StockPrice(FinancialStatements):
     
     def load(self,stock):
         
-        self.stock = stock
-        data_name = self.check_stock()
-        if data_name ==0 :
+        self.stock = str( stock )
+        self.data_name = self.change_stock_name()
+        if self.data_name ==0 :
             return ''
         
-        self.get_col_name('StockPrice',data_name)
+        self.get_col_name()
     
-        data = pd.DataFrame()
+        self.data = pd.DataFrame()
         for j in range(len(self.col_name)):
             print(j)
             col = self.col_name[j]
-            text = 'select ' + col + ' from ' + data_name
-            
-            tem = execute_sql2(
-                host = host,
-                user = user,
-                password = password,
-                database = 'StockPrice',
-                sql_text = text)
-            
-            if col=='Date':
-                tem = [np.datetime64(x[0]) for x in tem]
-                tem = pd.DataFrame(tem)
-                data[col] = tem.loc[:,0]
-            else:
-                tem = np.concatenate(tem, axis=0)
-                tem = pd.DataFrame(tem)
-                data[col] = tem
+            text = 'select ' + col + ' from ' + self.data_name
+            self.execute2sql(text,col)
                 
-        return data
+        return self.data
     
-
 #--------------------------------------------------------------- 
-''' test
+''' test StockInfo
 SI = StockInfo()
 data = SI.load()
 
 '''
-class StockInfo(FinancialStatements):
-    #---------------------------------------------------------------    
-    def load(self):                        
-        
-        self.get_col_name('Financial_DataSet','StockInfo')
-        #---------------------------------------------------------------   
-        data = pd.DataFrame()
-        for j in range(len(self.col_name)):
-            print(j)
-            col = self.col_name[j]
-            text = 'select ' + col + ' from ' + '`StockInfo`'
-            
-            tem = execute_sql2(
-                host = host,
-                user = user,
-                password = password,
+class StockInfo(LoadDate):
+    def __init__(self):
+        super(StockInfo, self).__init__(
                 database = 'Financial_DataSet',
-                sql_text = text)
-            
-            if col=='Date':
-                tem = [np.datetime64(x[0]) for x in tem]
-                tem = pd.DataFrame(tem)
-                data[col] = tem.loc[:,0]
-            else:
-                tem = np.concatenate(tem, axis=0)
-                tem = pd.DataFrame(tem)
-                data[col] = tem.T.iloc[0]
-        return data
+                data_name = 'FinancialStatements')
+        self.data_name = 'StockInfo'
+        
+    def load(self):                        
+        self.get_col_name()
+        #---------------------------------------------------------------   
+        self.data = pd.DataFrame()
+        self.data = self.get_data(all_data='T')
+                
+        return self.data
     
+#--------------------------------------------------------------- 
+''' test
+SD = StockDividend()
+data = SD.load('2330')
+
+'''
+class StockDividend(LoadDate):
+    def __init__(self):
+        super(StockDividend, self).__init__(
+                database = 'Financial_DataSet',
+                data_name = 'StockDividend')
+        #self.data_name = 'StockInfo'    
+ 
 
 
