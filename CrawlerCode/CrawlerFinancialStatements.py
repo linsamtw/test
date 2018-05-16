34e9cc47262028f36b4b2f115bbc7f4e2f4d9ee6
lin sam <610411102@ems.ndhu.edu.tw>
2018-05-16 16:32:07 +0800

    Add files via upload

--- CrawlerCode/CrawlerFinancialStatements.py
+++ CrawlerCode/CrawlerFinancialStatements.py
@@ -1,10 +1,4 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Apr 25 19:55:24 2018
 
-@author: linsam
-"""
 '''
 0 REV：營業收入 (Gross Revenue)
 1 COST：營業成本 (Cost of Goods Sold or Manufacturing)
@@ -30,14 +24,18 @@ import re
 import pandas as pd
 import numpy as np
 import pymysql
-import copy
+import datetime
 os.chdir('/home/linsam/project/Financial_Crawler')
 sys.path.append('/home/linsam/project/Financial_Crawler')
 import stock_sql
 import FinancialKey
+import load_data
 
+#------------------------------------------------------------------------------
+host = FinancialKey.host
+user = FinancialKey.user
+password = FinancialKey.password
 # url = 'https://stock.wearn.com/Income_detial.asp?kind=2317&y=10604'
-
 # self = Crawler2SQL(host,user,password,self.stock_financial_statements)
 class Crawler2SQL:
 
@@ -82,10 +80,10 @@ class Crawler2SQL:
         # database = 'Financial_DataSet'
         conn = ( pymysql.connect(host = self.host,# SQL IP
                          port = 3306,
-                         user = self.user,# 帳號
-                         password = self.password,# 密碼
-                         database = database,  # 資料庫名稱
-                         charset="utf8") )   #  編碼
+                         user = self.user,
+                         password = self.password,
+                         database = database,  
+                         charset="utf8") )   
 
         upload_string = ('insert into ' + dataset_name + 
          '( BTAXM, COST, EPS, GM, NI, BTAX, NIM, OE, OI, OIM,' +
@@ -118,7 +116,6 @@ class Crawler2SQL:
              
         conn.commit()
         conn.close()     
-        
         
 # self = CrawlerFinancialStatements(stock_id_set)
 class CrawlerFinancialStatements:
@@ -154,9 +151,9 @@ class CrawlerFinancialStatements:
     #--------------------------------------------------
     def take_stock_value(self,url):
         
-        res = requests.get(url,verify = True)  # 擷取該網站 原始碼         
+        res = requests.get(url,verify = True)      
         res.encoding = 'big5'
-        soup = BeautifulSoup(res.text, "lxml")# beautiful 漂亮的呈現原始碼
+        soup = BeautifulSoup(res.text, "lxml")
         
         index = [0,1,2,3,5,6,7,
                  10,11,13,14,15,17]
@@ -273,10 +270,110 @@ class CrawlerFinancialStatements:
             fix_data.index = range(len(fix_data))
             self.stock_financial_statements = fix_data
             del fix_data
+#----------------------------------------------------------------------------------------------------
+class AutoCrawlerFinancialStatements(CrawlerFinancialStatements):
+    def __init__(self,host,user,password,database):
+        self.host = host
+        self.user = user
+        self.password = password
+        self.database = database
         
-# https://www.cnyes.com/twstock/financial11.aspx?pi=2&mtype=T&stype=24&year=2016
+    def get_stock_id_set(self):
+        
+        data =  load_data.execute_sql2(
+                host = self.host,
+                user = self.user,
+                password = self.password,
+                database = self.database,
+                sql_text = 'SELECT distinct `stock_id` FROM FinancialStatements')
+        
+        self.stock_id_set = [ da[0] for da in data]
+    
+    def get_yearquar(self,stock):
+        
+        sql_text = "SELECT year,quar FROM FinancialStatements  WHERE stock_id = "
+        sql_text = sql_text + stock 
+        
+        tem =  load_data.execute_sql2(
+                host = self.host,
+                user = self.user,
+                password = self.password,
+                database = self.database,
+                sql_text = sql_text)
+        year,quar = [],[]
+        for te in tem:
+            year.append(te[0]-1911)
+            quar.append(te[1])
 
-def main():
+        return year,quar
+
+    def crawler(self):
+
+        stock_financial_statements = pd.DataFrame()        
+        for i in range(len(self.url)) :# 
+            print(str(i)+'/'+str(len(self.url)))
+            url = self.url[i]
+            stock_value,bo = self.take_stock_value(url)
+            if bo == 0:
+                print('error')
+            elif bo == 1:
+                stock_value['stock_id'] = self.take_sotck_id(url)
+                stock_value['year'],stock_value['quar'] = self.take_year_and_quar(url)
+                stock_value['url'] = url
+                stock_financial_statements = stock_financial_statements.append(stock_value)
+        if len(stock_financial_statements) > 0:
+            stock_financial_statements = stock_financial_statements.sort_values(['stock_id','year','quar'])
+            stock_financial_statements.index = range(len(stock_financial_statements))
+        #self.stock_financial_statements = stock_financial_statements
+        return stock_financial_statements
+
+    def crawler_new_data(self):
+        def get_new_url(tem,date):
+            url = []
+            for i in range(len(tem)):
+                if 'Income_detial.asp' in str(tem[i]):
+                    #print(i)
+                    x = tem[i].text
+                    y = int( re.search('[0-9]+年',x).group(0).replace('年','') )
+                    q = int( re.search('[0-9]+季',x).group(0).replace('季','') )
+                    da = y*10 + q
+                    if da not in date:
+                        url.append( 'https://stock.wearn.com/' + tem[i]['href'] )    
+            return url
+        #------------------------------------------------------------------------------
+        #------------------------------------------------------------------------------
+        self.stock_financial_statements = pd.DataFrame()
+        for i in range(len(self.stock_id_set)):
+            print(str(i)+'/'+str(len(self.stock_id_set)))
+            stock = self.stock_id_set[i]
+            year,quar = self.get_yearquar(stock)
+            date = [year[i]*10 + quar[i] for i in range(len(year)) ]
+            
+            index_url = 'https://stock.wearn.com/income.asp?kind='
+            index_url = index_url + stock
+      
+            res = requests.post(index_url,verify = True)  
+            res.encoding = 'big5'
+            soup = BeautifulSoup(res.text, "lxml")#
+            tem = soup.find_all('a') 
+            
+            self.url = get_new_url(tem,date)
+            if self.url == []:
+                123
+            else:
+                tem = self.crawler()
+                self.stock_financial_statements = self.stock_financial_statements.append(tem)
+        if len(self.stock_financial_statements) > 0:
+            self.stock_financial_statements = self.stock_financial_statements.sort_values(['stock_id','year','quar'])
+            self.stock_financial_statements.index = range(len(self.stock_financial_statements))
+            self.stock_financial_statements['year'] = self.stock_financial_statements['year'] + 1911
+            
+    def main(self):
+        self.get_stock_id_set()
+        self.crawler_new_data()
+#----------------------------------------------------------------------------------------------------
+#----------------------------------------------------------------------------------------------------
+def crawler_history():
     
     stock_info = stock_sql.get_stock_info_by_sql()
     stock_id_set = stock_info.stock_id
@@ -285,10 +382,6 @@ def main():
     CFS.crawler()
     CFS.fix()
     CFS.stock_financial_statements['year'] = CFS.stock_financial_statements['year'] + 1911
-
-    host = FinancialKey.host
-    user = FinancialKey.user
-    password = FinancialKey.password
     
     sql = Crawler2SQL(host,user,password,CFS.stock_financial_statements)
     #sql = Crawler2SQL(host,user,password,data)
@@ -300,6 +393,52 @@ def main():
     dataset_name = 'FinancialStatements'
     database = 'Financial_DataSet'
     sql.upload2sql(database,dataset_name )
+
+def auto_crawler_new():
+    ACFS = AutoCrawlerFinancialStatements(host = host,
+                                          user = user,
+                                          password = password,
+                                          database = 'Financial_DataSet')
+    # self = ACFS
+    ACFS.main()
+    if len(ACFS.stock_financial_statements) != 0 :    
+        try:
+            ACFS.fix()
+        except:
+            123
+            
+        if ACFS.stock_financial_statements.columns[0] == 0:
+            ACFS.stock_financial_statements = ACFS.stock_financial_statements.T
+            
+        sql = Crawler2SQL(host,user,password,ACFS.stock_financial_statements)
+        sql.upload2sql(dataset_name = 'FinancialStatements',database = 'Financial_DataSet' )
+
+    #------------------------------------------------------
+    try:
+        sql_string = 'create table FinancialStatements ( name text(100),FSdate datetime)'
+        FinancialKey.creat_datatable(host,user,password,'python',sql_string,'FinancialStatements')
+    except:
+        123
+    text = 'insert into FinancialStatements (name,FSdate) values(%s,%s)'
+    tem = str( datetime.datetime.now() )
+    time = re.split('\.',tem)[0]
+    value = ('FinancialStatements',time)
+
+    stock_sql.Update2Sql(host,user,password,
+                         'python',text,value)   
+    
+    
+def main(x):
+    if x == 'history':
+        crawler_history()
+    elif x == 'new':
+        # python3 /home/linsam/project/Financial_Crawler/CrawlerFinancialStatements.py new
+        auto_crawler_new()
+    
+if __name__ == '__main__':
+    x = sys.argv[1]# cmd : input new or history
+    main(x)
+
 
 
 
